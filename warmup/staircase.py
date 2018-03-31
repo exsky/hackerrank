bc668aedaa11a793ff065e4ae70e858fc1af66c6
exsky <alex19911118@gmail.com>
2018-03-31 18:47:50 +0800

    Staircase https://www.hackerrank.com/challenges/staircase/problem

---
+++ warmup/staircase.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import os
+import sys
+
+def staircase(n):
+    for i in range( 1, n+1 ):
+        print(' '*(n-i)+'#'*i)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
+
